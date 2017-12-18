f7c5870350edf9b8e205271e713b9a625735d3b6
Ben Zelnick <34197604+ZelnickB@users.noreply.github.com>
2017-12-18 09:00:02 -0500

    Major Improvements
    Many improvements. See individual documents for details.

--- Script.js
+++ Script.js
@@ -1,3 +1,24 @@
+var isfullscreen = false
 function fullscreen() {
-    alert("test.");
+    if(isfullscreen == false){
+        document.getElementById("toolsheader").hidden = "true";
+        document.getElementById("runscript").hidden = "true";
+        document.getElementById("browserheader").hidden = "true";
+        document.getElementById("browserfullscreen").value = "Exit Fullscreen";
+        document.getElementById("frame").style = "height: 435px!important;"
+        isfullscreen = true;
+    }
+        else{
+            document.getElementById("toolsheader").removeAttribute("hidden");
+            document.getElementById("runscript").removeAttribute("hidden");
+            document.getElementById("browserheader").removeAttribute("hidden");
+            document.getElementById("frame").removeAttribute("style");
+            document.getElementById("browserfullscreen").value = "Fullscreen";
+            isfullscreen = false;
+        }
+}
+function nav() {
+    document.getElementById("frame").src = document.getElementById("navurl").value
+    document.getElementById("currentURL").innerHTML = "Current URL: " + document.getElementById("navurl").value
+    document.getElementById("navurl").value = ""
 }
