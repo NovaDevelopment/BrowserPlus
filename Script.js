8437c79322962408a21c818a0510a856672cf23e
Ben Zelnick <34197604+ZelnickB@users.noreply.github.com>
2017-12-29 13:17:49 -0500

    Added Disclaimer

--- Script.js
+++ Script.js
@@ -1,13 +1,17 @@
+if (document.cookie.replace(/(?:(?:^|.*;\s*)disclaimerDisplayed\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+    alert("By using this site, you agree that you have read and understand the Disclaimers and Terms section of the document, README.md, in the GitHub repository that contains the contents of this site. If you have not done so, you must complete this action before using the site. Navigate to https://zelnickb.github.io/BrowserPlus/README.md in order to read the document. IT IS YOUR RESPONSIBILITY TO CONVERT THE MARKDOWN TO A MORE READABLE FORM IF YOU CANNOT COMPREHEND THE CODE.");
+    document.cookie = "disclaimerDisplayed=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+}
 var isfullscreen = false;
 var isblurred = false;
 function fullscreen() {
-    if(isfullscreen == false){
+    if(isfullscreen == false) {
         document.getElementById("non-browser").hidden = "true";
         document.getElementById("browserfullscreen").value = "Exit Fullscreen";
         document.getElementById("frame").style = "height: 465px!important;";
         isfullscreen = true;
     }
-        else{
+        else {
             document.getElementById("non-browser").removeAttribute("hidden");
             document.getElementById("frame").removeAttribute("style");
             document.getElementById("browserfullscreen").value = "Fullscreen";
@@ -15,11 +19,11 @@ function fullscreen() {
         }
 }
 function nav() {
-    if(document.getElementById("navurl").value == ""){
+    if(document.getElementById("navurl").value == "") {
         alert("Please enter a URL to navigate.")
         document.getElementById("navurl").style = "background-color: red!important; color: white!important"
     }
-        else{
+        else {
             document.getElementById("browsertabtitle").innerHTML = "Loading...";
             document.getElementById("frame").hidden = "true";
             document.getElementById("loading").removeAttribute("hidden");
