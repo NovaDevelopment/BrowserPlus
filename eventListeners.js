1e3289adae3be29a5f8c82fb47fc1c0cd30b3120
Ben Zelnick <34197604+ZelnickB@users.noreply.github.com>
2019-03-23 12:59:42 -0400

    General Fixes and Enhancements
    * Added whitespace in /eventListeners.js
    * Replaced missing HTML ID attributes in /index.html
    * Added to the console welcome message for developers
    * Improved splash page performance

--- eventListeners.js
+++ eventListeners.js
@@ -1,7 +1,16 @@
 document.getElementById("blur").addEventListener("click", blurcontent, false);
 document.getElementById("windowClose").addEventListener("click", windowclose, false);
 document.getElementById("quickRemove").addEventListener("click", clearData, false);
-document.getElementById("activateFullScreen").addEventListener("click", function() {document.getElementById("html").webkitRequestFullscreen()}, false);
+document.getElementById("activateFullScreen").addEventListener("click", function() {
+    if(document.fullscreenElement === null) {
+        document.getElementById("html").requestFullscreen();
+        document.getElementById("activateFullScreen").value = "Exit Fullscreen";
+    }
+    else {
+        document.exitFullscreen();
+        document.getElementById("activateFullScreen").value = "Activate Fullscreen";
+    }
+}, false);
 document.getElementById("toggleDarkMode").addEventListener("click", toggleDarkMode, false);
 document.getElementById("toggleSimpleMode").addEventListener("click", toggleSimpleMode, false);
 document.getElementById("toggleCoffeeMode").addEventListener("click", toggleCoffeeMode, false);
@@ -10,6 +19,8 @@ document.getElementById("versionInfoViewer").addEventListener("click", versionIn
 document.getElementById("openGHPage").addEventListener("click", aboutInBrowser, false);
 document.getElementById("reportIssues").addEventListener("click", reportIssues, false);
 document.getElementById("browserfullscreen").addEventListener("click", fullscreen, false);
-document.getElementById("navigationbutton").addEventListener("click", function() {nav(document.getElementById("navurl").value)}, false);
+document.getElementById("navigationbutton").addEventListener("click", function() {
+    nav(document.getElementById("navurl").value)
+}, false);
 document.getElementById("refreshbutton").addEventListener("click", refreshiframe, false);
 document.getElementById("frame").addEventListener("load", loadedframe, false);
